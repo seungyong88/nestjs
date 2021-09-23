22026f555be869b918fd05fdfad2915e4263974e
seungyong88 <gosng2001@naver.com>
2021-09-23 19:49:22 +0900

    다시 한거

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get('/')
+  getAllBoards(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post('/')
+  createBoard(
+    @Body() createBoardDto:CreateBoardDto
+  ): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardId(
+    @Param('id') id: string
+  ): Board {
+    console.log('ddddd ', id);
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): string {
+    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@", id)
+    this.boardsService.deleteBoard(id);
+    return id;
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus
+  ) {
+    return this.boardsService.updateBoardStatus(id, status);
+  }
+
+}
