dbdfd59da607fabcf9b606a0a618be91776eae57
seungyong88 <gosng2001@naver.com>
2022-01-16 19:36:41 +0900

    push push!

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Board } from './board.model';
+import { BoardsService } from "./boards.service";
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get('/')
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post('/')
+  createBoard(
+    // @Body('title') title: string, 
+    // @Body('description') description: string,
+    @Body() createdBoardDto: CreateBoardDto 
+  ): Board {
+    return this.boardsService.createBoard(createdBoardDto);
+  }
+
+
+}
