76b49c3ec471153d08de61099641aaed0fc61a94
seungyong <gosng2001@naver.com>
2022-01-21 18:35:13 +0900

    auth: 회원가입

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+  imports: [
+    TypeOrmModule.forFeature([UserRepository])
+  ],
+  providers: [AuthService],
+  controllers: [AuthController]
 })
 export class AuthModule {}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,12 @@
+import { User } from "src/auth/user.entity";
+import { EntityRepository, Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credential.dto";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
+}
