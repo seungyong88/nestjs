fab759233fa779aee3aa6eee06bb4e732eea046d
seungyong <gosng2001@naver.com>
2022-01-24 17:52:11 +0900

    push

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,36 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
- constructor(
-   @InjectRepository(UserRepository)
-   private userRepository: UserRepository
- ) {}
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+    private jwtService: JwtService
+  ) { }
 
- createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-   return this.userRepository.createUser(authCredentialsDto);
- }
+  createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.userRepository.findOne({ username });
+
+    if(user && (await bcrypt.compare(password, user.password))) {
+      // usertoken 생성 { Secret + Payload }
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
+    }else{
+      throw new UnauthorizedException('login failed');
+    }
+
+    // return this.userRepository.signIn(authCredentialsDto);
+  }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,12 +1,27 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { User } from "src/auth/user.entity";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
-    await this.save(user);
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+    
+    try {
+      await this.save(user);
+    } catch(e) {
+      if(e.code === "23505") {
+        throw new ConflictException(e.detail);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
